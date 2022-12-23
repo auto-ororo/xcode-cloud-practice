7c773f8744dccbb6321e8914ac0eff11a6264d5f
Ryo Narisawa <ororoauto@gmail.com>
2022-12-24 01:41:07 +0900

    Lint成功になるように変更

--- Package/Sources/Content/ContentView.swift
+++ Package/Sources/Content/ContentView.swift
@@ -20,10 +20,6 @@ public struct ContentView: View {
                 Text("環境：")
                 Text(envName)
             }
-                        
-            
-            
-            
             .padding(.vertical, 16)
             HStack {
                 Text("SecretKey：")
